9fdbe5b0acda7e3b02e1baa7b5356ceb2ea39775
blurk <nsinh6745@gmail.com>
2025-06-10 18:50:23 +0700

    learn-adonis: 2.16

---
+++ learn-adonis/resources/movies/movie-aac.md
@@ -0,0 +1,8 @@
+---
+title: Movie 3
+summary: Lorem ipsum dolor sit, amet consectetur adipisicing elit
+---
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam quaerat cupiditate error.
+Praesentium illum laudantium quis aspernatur ratione accusamus, id temporibus? Quaerat beatae
+nihil facere culpa necessitatibus vel adipisci iure.
---
+++ learn-adonis/start/routes.ts
@@ -0,0 +1,23 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+const MoviesController = () => import('#controllers/movies_controller')
+import RedisController from '#controllers/redis_controller'
+import router from '@adonisjs/core/services/router'
+
+router.get('/', [MoviesController, 'index']).as('home')
+
+router
+  .get('/movies/:slug', [MoviesController, 'show'])
+  .as('movies.show')
+  .where('slug', router.matchers.slug())
+
+// Use /flush as default and :slug later
+router.get('/redis/flush', [RedisController, 'flush']).as('redis.flush')
+router.get('/redis/:slug', [RedisController, 'destroy']).as('redis.destroy')
